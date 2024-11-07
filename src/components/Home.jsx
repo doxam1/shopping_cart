0b444e921f37ceb52216bc29455c22035b70f225
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-11-07 23:43:46 +0200

    header, react-icons, some styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+
+const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+  background: url("img/rushBrain.webp") center/cover;
+  height: calc(100vh - 6em);
+`;
+
+const H2 = styled.h2`
+  color: #fff;
+  background-color: black;
+  padding: 1em;
+  border-radius: 1em;
+`;
+
 const Home = () => {
-  return <div>Welcome home beach!</div>;
+  return (
+    <Wrapper>
+      <H2>We&apos;ve got everything you need!</H2>
+    </Wrapper>
+  );
 };
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,86 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { FaHome } from "react-icons/fa";
+import { GiRotaryPhone } from "react-icons/gi";
 
 const Wrapper = styled.div`
-  padding: 2em;
-  background: blue;
+  /* padding: 1rem; */
   display: flex;
+  justify-content: flex-start;
+  align-items: center;
   gap: 1em;
-  place-content: center;
+  font-size: 21px;
 
-  & a {
-    padding: 0.4em 0.8em;
-    color: black;
+  a {
+    padding: 0.4em 0.6em;
+    color: #424242;
     text-decoration: none;
+
+    &:not(.icon) {
+      /* margin: 0 auto; */
+      font-size: large;
+    }
+  }
+
+  & a:hover {
+    opacity: 75%;
+    &:not(.icon) {
+      text-decoration-line: underline;
+      text-underline-offset: 1rem;
+      color: #053c4e;
+    }
+  }
+
+  .icon {
+    margin-right: 4rem;
+    margin-left: 1rem;
+    font-size: 2.5rem;
+  }
+
+  /* & a:not(.icon) {
+    padding: 0.4em 0.8em;
+    color: #aaa;
+    text-decoration: none;
+
+    &:hover {
+      
+    }
+  } */
+`;
+
+const Header = styled(Wrapper)`
+  background-color: #053c4e;
+  font-size: medium;
+
+  p {
+    color: #fff;
+    margin: 0.2rem auto;
   }
 `;
 const navLinkActiveStyle = ({ isActive }) => {
   return {
-    background: isActive ? "black" : "",
-    color: isActive ? "white" : "",
+    color: isActive ? "#077599" : "",
   };
 };
 const Navbar = () => {
   return (
-    <Wrapper>
-      <NavLink to="/" style={navLinkActiveStyle}>
-        Home
-      </NavLink>
-      <NavLink to="shop" style={navLinkActiveStyle}>
-        Shop
-      </NavLink>
-    </Wrapper>
+    <>
+      <Header>
+        <p>
+          Need help? please call us{" "}
+          <GiRotaryPhone style={{ position: "relative", top: "3px" }} />{" "}
+          972-3-1123312
+        </p>
+      </Header>
+      <Wrapper>
+        <NavLink to="/" className={"icon"}>
+          <FaHome />
+        </NavLink>
+        <NavLink to="shop" style={navLinkActiveStyle}>
+          &nbsp;&nbsp;Shop&nbsp;&nbsp;
+        </NavLink>
+      </Wrapper>
+    </>
   );
 };
 
